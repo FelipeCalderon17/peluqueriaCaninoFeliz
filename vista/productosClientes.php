c63da36c63422565b3bb718d71a352a21249347d
Mams117 <adsoadso17@gmail.com>
2023-11-18 14:33:29 -0500

    mario

--- vista/productosClientes.php
+++ vista/productosClientes.php
@@ -128,17 +128,7 @@
         </div>
     </nav>
     <!-- END nav -->
-    <div class="hero-wrap js-fullheight" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
-        <div class="overlay"></div>
-        <div class="container">
-            <div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
-                <div class="col-md-11 ftco-animate text-center">
-                    <h1 class="mb-4">¿Quieres que tu mascota tenga una buena experiencia?</h1>
-                    <p><a href="#" class="btn btn-primary mr-md-4 py-3 px-4">Conocer mas <span class="ion-ios-arrow-forward"></span></a></p>
-                </div>
-            </div>
-        </div>
-    </div>
+
 
 
     <section id="productos" class="ftco-section ftco-no-pt ftco-no-pb">
